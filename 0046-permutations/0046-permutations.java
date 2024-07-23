95e7e991162663302e6fcaa27844d1cb67273259
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2024-07-23 13:05:51 +0530

    Time: 2 ms (42.10%), Space: 44.5 MB (40.50%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,23 +2,23 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> li  = new ArrayList<>();
-        int flag[] = new int[nums.length];
-        helper(nums,ans,li,flag);
+        int visited[] = new int[nums.length];
+        helper(nums,ans,li,visited);
         return ans;
         
     }
-    public static void helper(int[] nums , List<List<Integer>> ans , List<Integer> li , int[] flag){
+    public static void helper(int[] nums , List<List<Integer>> ans , List<Integer> li , int[] visited){
         if(li.size() == nums.length){
             ans.add(new ArrayList<>(li));
             return;
         }
-        for(int i=0 ; i<flag.length ; i++){
-            if(flag[i]!=1){
-                flag[i] = 1;
+        for(int i=0 ; i<visited.length ; i++){
+            if(visited[i]!=1){
+                visited[i] = 1;
                 li.add(nums[i]);
-                helper(nums, ans , li , flag);
+                helper(nums, ans , li , visited);
                 li.remove(li.size()-1);
-                flag[i] = 0;
+                visited[i] = 0;
             }
         }
         
