d1f36844a58d9645846e3328d27e365e024a29ee
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2024-07-25 12:53:04 +0530

    Time: 2 ms (58.51%), Space: 41.5 MB (47.96%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,36 +1,29 @@
-import java.util.Stack;
-
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         Stack<Character> st1 = new Stack<>();
         Stack<Character> st2 = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '#') {
-                if (!st1.isEmpty()) {
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        for(char i : sa){
+            if(i=='#'){
+                if(!st1.isEmpty())
                     st1.pop();
-                }
-            } else {
-                st1.push(c);
             }
+            else
+                st1.push(i);
         }
-
-        for (char c : t.toCharArray()) {
-            if (c == '#') {
-                if (!st2.isEmpty()) {
+        for(char i : ta){
+            if(i=='#'){
+                if(!st2.isEmpty())
                     st2.pop();
-                }
-            } else {
-                st2.push(c);
             }
+            else
+                st2.push(i);            
         }
-
-        while (!st1.isEmpty() && !st2.isEmpty()) {
-            if (st1.pop() != st2.pop()) {
-                return false;
-            }
+        while(!st1.isEmpty() && !st2.isEmpty()){
+            if(st1.pop() != st2.pop())
+                    return false;
         }
-
-        return st1.isEmpty() && st2.isEmpty();
+        return (st1.isEmpty() && st2.isEmpty());
     }
 }
