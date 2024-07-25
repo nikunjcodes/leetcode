c185057924cc0ec24914ac05507f09683a9ce74b
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2024-07-25 12:49:05 +0530

    Time: 2 ms (58.51%), Space: 41.3 MB (87.06%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '#') {
+                if (!st1.isEmpty()) {
+                    st1.pop();
+                }
+            } else {
+                st1.push(c);
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (c == '#') {
+                if (!st2.isEmpty()) {
+                    st2.pop();
+                }
+            } else {
+                st2.push(c);
+            }
+        }
+
+        while (!st1.isEmpty() && !st2.isEmpty()) {
+            if (st1.pop() != st2.pop()) {
+                return false;
+            }
+        }
+
+        return st1.isEmpty() && st2.isEmpty();
+    }
+}
