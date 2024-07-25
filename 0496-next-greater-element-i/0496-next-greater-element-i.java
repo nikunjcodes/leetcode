6d7cf1f875cbbcad834b46cb4b9fcbe835dceb3d
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2024-07-26 01:46:36 +0530

    Time: 2 ms (99.17%), Space: 43.2 MB (75.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        int max=-99999;
+        for(int i=0 ; i<nums2.length ; i++){
+            if(nums2[i]>max)
+                max= nums2[i];
+        }
+        int[] hash = new int[max+1];
+        for(int i=nums2.length -1 ; i>=0 ; i--){
+            while(!st.isEmpty() && st.peek()<=nums2[i]){
+                st.pop();
+            }
+            if(st.isEmpty())
+                hash[nums2[i]] = -1;
+            else
+                hash[nums2[i]] = st.peek();
+                st.push(nums2[i]);
+        }
         int[] ans = new int[nums1.length];
         for(int i=0 ; i<nums1.length ; i++){
-            int el = nums1[i];
-            int idx=0;
-            for(int j=0 ; j<nums2.length ; j++){
-                if(nums2[j]==el){
-                    idx=j;
-                    break;
-                }
-            }
-            for(int j=idx ; j<nums2.length ; j++){
-                if(nums2[j]>nums2[idx]){
-                    ans[i] = nums2[j];
-                    break;
-                }
-                ans[i] = -1;
-            }
+            ans[i] = hash[nums1[i]];
         }
-            return ans;
+        return ans;
 
     }
 }
