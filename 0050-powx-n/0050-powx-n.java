eb98c467f12a81f021fb46e287607dbbdb39b6b8
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2024-07-30 21:27:44 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (47.22%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long temp = n;
+        if(temp<0)
+            temp=temp*-1;
+        while(temp>0){
+            if(temp%2==0){
+                x=x*x;
+                temp=temp/2;
+            }
+            else if (temp%2==1){
+                ans=ans*x;
+                temp=temp-1;
+            }
+        }
+        if(n<0)
+            ans=  (double)1.0/(double)ans;
+        return ans;
+    }
+}
