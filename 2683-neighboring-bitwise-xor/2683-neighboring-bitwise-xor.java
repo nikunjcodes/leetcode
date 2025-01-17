0d5b31ed3acb56ad9c8b75afa3bdb74b6180b61b
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2025-01-17 12:54:57 +0530

    Time: 2 ms (100%), Space: 62.4 MB (40.59%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int xor=0;
+        for(int num : derived)
+            xor = xor^num;
+        return xor==0;
+    }
+} 
