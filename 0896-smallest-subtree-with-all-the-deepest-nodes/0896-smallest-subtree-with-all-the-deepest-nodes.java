d7f368c8b9f17e20fc82b17e9ea7f2a0b88971d5
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2025-04-04 20:36:30 +0530

    Time: 0 ms (100%), Space: 41.7 MB (73.19%) - LeetHub

---
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return helper(root).getValue();
+    }
+    public Pair<Integer , TreeNode> helper(TreeNode root){
+        if(root==null)
+            return new Pair(0,null);
+        Pair<Integer, TreeNode> left = helper(root.left) , right=  helper(root.right);
+        int d1 = left.getKey();
+        int d2 = right.getKey();
+        return new Pair(Math.max(d1,d2)+1 , d1==d2 ? root : d1 > d2 ? left.getValue() : right.getValue());
+    }
+}
