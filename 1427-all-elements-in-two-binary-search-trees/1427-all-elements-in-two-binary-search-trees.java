0d2509618dbd5987dab1669745a4037ea9a260bf
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2025-07-10 10:04:38 +0530

    1427-ALL-ELEMENTS-IN-TWO-BINARY-SEARCH-TREES Stats: Time: 16 ms (90.33%), Space: 46.2 MB (77.87%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -15,35 +15,35 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        getAllElements(list1 , root1);
-        getAllElements(list2,  root2);
-        List<Integer> ans = new  ArrayList<>();
-        int i=0 , j=0;
+        List<Integer> inorder1 = new ArrayList<>();
+        List<Integer> inorder2 = new ArrayList<>();
+        inorder(root1  , inorder1);
+        inorder(root2 , inorder2);
+        return merge(inorder1 , inorder2);
+    }
+    void inorder(TreeNode node , List<Integer> list){
+        if(node==null)
+            return;
+        inorder(node.left , list);
+        list.add(node.val);
+        inorder(node.right , list);
+    }
+    List<Integer> merge(List<Integer> list1 , List<Integer> list2){
+        List<Integer> ans  = new ArrayList<>();
+        int i=0;
+        int j=0;
         while(i<list1.size() && j<list2.size()){
-            if(list1.get(i) <list2.get(j)){
-                ans.add(list1.get(i));
-                i++;
-            }
-            else{
+            if(list1.get(i) > list2.get(j)){
                 ans.add(list2.get(j++));
             }
+            else
+                ans.add(list1.get(i++));
         }
         while(i<list1.size()){
             ans.add(list1.get(i++));
         }
-        while(j<list2.size()){
+        while(j<list2.size())
             ans.add(list2.get(j++));
-        }
         return ans;
-
-    }
-    void getAllElements(List<Integer> list , TreeNode root){
-        if(root==null)
-            return;
-        getAllElements(list , root.left);
-        list.add(root.val);
-        getAllElements(list , root.right);
     }
 }
