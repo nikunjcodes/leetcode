f78c50716aa0c469ed2679fe95d490e0dae3b08f
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2025-07-14 10:24:00 +0530

    1753-PATH-WITH-MINIMUM-EFFORT Stats: Time: 74 ms (18.54%), Space: 45.4 MB (53.91%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -1,40 +1,38 @@
 class Solution {
     class Pair{
-        int row;
-        int col;
+        int x;
+        int y;
         int diff;
-        public Pair(int row , int col, int diff){
-            this.row = row;
-            this.col = col;
-            this.diff=  diff;
+        public Pair(int x , int y, int diff){
+            this.x = x;
+            this.y = y;
+            this.diff = diff;
         }
     }
     public int minimumEffortPath(int[][] heights) {
-        int[][] dirs = new int[][] {{-1,0} , {0,-1} , {1,0} , {0,1}};
-        int n = heights.length;
-        int m = heights[0].length;
-        boolean[][] vis = new boolean[heights.length][heights[0].length];
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> a.diff- b.diff);
+        int[][] dirs = new int[][] {{1,0} , {0,1} , {-1,0}  , {0,-1}};
+        int rows = heights.length;
+        int cols = heights[0].length;
+        boolean[][] vis = new boolean[rows][cols];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> a.diff - b.diff);
         pq.offer(new Pair(0,0,0));
+        vis[0][0] = true;
         while(!pq.isEmpty()){
             Pair p = pq.poll();
-            int row = p.row;
-            int col = p.col;
-            int abs  = p.diff;
-            if(row==n-1 && col == m-1)
-                return abs;
-            if(!vis[row][col]){
-                vis[row][col] = true;
+            int x = p.x;
+            int y = p.y;
+            int diff = p.diff;
+            vis[x][y] = true;
+            if(x==rows-1 && y==cols-1)
+                return diff;
             for(int[] dir : dirs){
-                int nRow = row + dir[0];
-                int nCol = col + dir[1];
-                if(nRow >=0 && nCol >=0 && nRow < n && nCol < m && !vis[nRow][nCol]){
-                    int diff = Math.abs(heights[row][col] - heights[nRow][nCol]);
-                    int nweE = Math.max(diff , abs);
-                    pq.add(new Pair(nRow , nCol , nweE));
-
+                int newX = x+dir[0];
+                int newY = y+dir[1];
+                if(newX >=0 && newY >=0 && newX<rows && newY<cols && !vis[newX][newY]){
+                    int eff =Math.abs(heights[newX][newY] - heights[x][y]);
+                    eff  = Math.max(eff , diff);
+                    pq.offer(new Pair(newX , newY , eff));
                 }
-            }
             }
         }
         return 0;
