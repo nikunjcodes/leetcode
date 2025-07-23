f44b92e4218e10eebd1aa38192aef494a9f83403
Nikunj  Jakhotiya <149929909+nikunjcodes@users.noreply.github.com>
2025-07-23 14:14:42 +0530

    Time: 4 ms (95.77%), Space: 42.2 MB (73.24%) - LeetHub

--- 2601-number-of-great-partitions/2601-number-of-great-partitions.java
+++ 2601-number-of-great-partitions/2601-number-of-great-partitions.java
@@ -1,16 +1,20 @@
-    class Solution{
-    public int countPartitions(int[] A, int k) {
-        long mod = (long)1e9 + 7, total = 0, res = 1, dp[] = new long[k];
-        dp[0] = 1;
-        for (int a : A) {
-            for (int i = k - 1 - a; i >= 0; --i)
-                dp[i + a] = (dp[i + a] + dp[i]) % mod;
-            res = res * 2 % mod;
-            total += a;
+class Solution {
+    public int countPartitions(int[] nums, int k) {
+        long mod = (long)1e9 + 7;
+        long tota=0;
+        long res=1;
+        long[] dp = new long[k];
+        dp[0] =1;
+        for(int num : nums){
+            for(int i=k-num-1 ; i>=0 ; i--){
+                dp[num + i] = (dp[num+i] + dp[i])%mod;
+            }
+            res = res*2 %mod;
+            tota+=num;
         }
-        for (int i = 0; i < k; ++i) {
-            res -= total - i < k ? dp[i] : dp[i] * 2;
+        for(int i=0 ; i<k; i++){
+            res-= tota-i <k ? dp[i] : dp[i]*2;
         }
-        return (int)((res % mod + mod) % mod);
+        return (int)((res%mod + mod)%mod);
     }
-    }
+}
